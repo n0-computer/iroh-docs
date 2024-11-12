dc28b4d2d83898e20c2d75d3dc5c8bdf4313d022
rklaehn <rklaehn@users.noreply.github.com>
2024-11-12 15:16:53 +0000

    deploy: 771844147f65aeee2d72fccf3894f637feba8d50

--- pr/5/docs/trait.impl/futures_core/stream/trait.Stream.js
+++ pr/5/docs/trait.impl/futures_core/stream/trait.Stream.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {
-"iroh_docs":[["impl Stream for <a class=\"struct\" href=\"iroh_docs/rpc/client/struct.ExportFileProgress.html\" title=\"struct iroh_docs::rpc::client::ExportFileProgress\">ExportFileProgress</a>"],["impl Stream for <a class=\"struct\" href=\"iroh_docs/rpc/client/struct.ImportFileProgress.html\" title=\"struct iroh_docs::rpc::client::ImportFileProgress\">ImportFileProgress</a>"]]
+"iroh_docs":[["impl Stream for <a class=\"struct\" href=\"iroh_docs/rpc/client/docs/struct.ExportFileProgress.html\" title=\"struct iroh_docs::rpc::client::docs::ExportFileProgress\">ExportFileProgress</a>"],["impl Stream for <a class=\"struct\" href=\"iroh_docs/rpc/client/docs/struct.ImportFileProgress.html\" title=\"struct iroh_docs::rpc::client::docs::ImportFileProgress\">ImportFileProgress</a>"]]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
