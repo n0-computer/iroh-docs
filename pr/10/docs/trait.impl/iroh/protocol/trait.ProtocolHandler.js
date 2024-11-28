ec8fd87de6f1a1f78d5ae3b23c61aad1c8fa0a73
dignifiedquire <dignifiedquire@users.noreply.github.com>
2024-11-28 12:45:58 +0000

    deploy: 4a9531b07fd863665b1d59923ce87e5dec236afb

---
+++ pr/10/docs/trait.impl/iroh/protocol/trait.ProtocolHandler.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {
+"iroh_docs":[["impl&lt;D: Store&gt; ProtocolHandler for <a class=\"struct\" href=\"iroh_docs/engine/struct.Engine.html\" title=\"struct iroh_docs::engine::Engine\">Engine</a>&lt;D&gt;"]]
+};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
