9d7d3abbedc05c14c3aada15e0c8b8f03e04cd9f
Ruediger Klaehn <rklaehn@protonmail.com>
2024-11-29 13:10:28 +0200

    fmt

--- src/lib.rs
+++ src/lib.rs
@@ -66,10 +66,10 @@ mod ranger;
 
 #[cfg(feature = "net")]
 #[cfg_attr(iroh_docsrs, doc(cfg(feature = "net")))]
-pub use self::ticket::DocTicket;
-pub use self::{heads::*, keys::*, sync::*};
+#[doc(inline)]
+pub use net::ALPN;
 
 #[cfg(feature = "net")]
 #[cfg_attr(iroh_docsrs, doc(cfg(feature = "net")))]
-#[doc(inline)]
-pub use net::ALPN;
+pub use self::ticket::DocTicket;
+pub use self::{heads::*, keys::*, sync::*};
