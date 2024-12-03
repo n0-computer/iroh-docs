cb98c90179cfa68f74a3566bbc90392ff15e34f9
dignifiedquire <dignifiedquire@users.noreply.github.com>
2024-12-03 10:33:24 +0000

    deploy: f197d0f306fc6c9faeb0895fae4bf6b69b3c36dd

---
+++ pr/13/docs/trait.impl/struct_iterable_internal/trait.Iterable.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {
+"iroh_docs":[["impl Iterable for <a class=\"struct\" href=\"iroh_docs/metrics/struct.Metrics.html\" title=\"struct iroh_docs::metrics::Metrics\">Metrics</a>"]]
+};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
