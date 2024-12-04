a730ae45c11c533e9f6a2cccd6ea97b0be5580c9
dignifiedquire <dignifiedquire@users.noreply.github.com>
2024-12-04 12:29:22 +0000

    deploy: 6d4612b81d8f858d5dcde78e65c65dd9b570caa8

---
+++ pr/14/docs/trait.impl/core/ops/drop/trait.Drop.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {
+"iroh_docs":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/ops/drop/trait.Drop.html\" title=\"trait core::ops::drop::Drop\">Drop</a> for <a class=\"struct\" href=\"iroh_docs/actor/struct.SyncHandle.html\" title=\"struct iroh_docs::actor::SyncHandle\">SyncHandle</a>"]]
+};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
