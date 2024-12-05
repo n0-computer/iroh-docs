b6c7616c401ac7c465beafebdc49f866263cf026
Rüdiger Klaehn <rklaehn@protonmail.com>
2024-12-05 17:17:33 +0200

    refactor: adapt to new ProtocolHandler (#15)

--- src/protocol.rs
+++ src/protocol.rs
@@ -1,7 +1,5 @@
 //! [`ProtocolHandler`] implementation for the docs [`Engine`].
 
-use std::sync::Arc;
-
 use anyhow::Result;
 use futures_lite::future::Boxed as BoxedFuture;
 use iroh::{endpoint::Connecting, protocol::ProtocolHandler};
@@ -9,13 +7,15 @@ use iroh::{endpoint::Connecting, protocol::ProtocolHandler};
 use crate::engine::Engine;
 
 impl<D: iroh_blobs::store::Store> ProtocolHandler for Engine<D> {
-    fn accept(self: Arc<Self>, conn: Connecting) -> BoxedFuture<Result<()>> {
-        Box::pin(async move { self.handle_connection(conn).await })
+    fn accept(&self, conn: Connecting) -> BoxedFuture<Result<()>> {
+        let this = self.clone();
+        Box::pin(async move { this.handle_connection(conn).await })
     }
 
-    fn shutdown(self: Arc<Self>) -> BoxedFuture<()> {
+    fn shutdown(&self) -> BoxedFuture<()> {
+        let this = self.clone();
         Box::pin(async move {
-            if let Err(err) = (*self).shutdown().await {
+            if let Err(err) = this.shutdown().await {
                 tracing::warn!("shutdown error: {:?}", err);
             }
         })
