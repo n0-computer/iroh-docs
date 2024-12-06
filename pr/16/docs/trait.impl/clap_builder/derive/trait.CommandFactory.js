b8cde6bf82148d402a8494102331ba1b5100cfac
dignifiedquire <dignifiedquire@users.noreply.github.com>
2024-12-06 11:07:15 +0000

    deploy: 0f6f9b0a89050894beff934aa976f9b8279a4ebd

---
+++ pr/16/docs/trait.impl/clap_builder/derive/trait.CommandFactory.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {
+"iroh_docs":[["impl CommandFactory for <a class=\"enum\" href=\"iroh_docs/cli/authors/enum.AuthorCommands.html\" title=\"enum iroh_docs::cli::authors::AuthorCommands\">AuthorCommands</a>"],["impl CommandFactory for <a class=\"enum\" href=\"iroh_docs/cli/enum.DocCommands.html\" title=\"enum iroh_docs::cli::DocCommands\">DocCommands</a>"]]
+};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
