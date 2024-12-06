ead3de2253f60841ac045413780279ac7522e8b6
rklaehn <rklaehn@users.noreply.github.com>
2024-12-06 15:43:06 +0000

    deploy: ae563a32aab961aea74f935e82ae37265df5b4dc

---
+++ pr/18/docs/trait.impl/quic_rpc/trait.Service.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {
+"iroh_docs":[["impl Service for <a class=\"struct\" href=\"iroh_docs/rpc/proto/struct.RpcService.html\" title=\"struct iroh_docs::rpc::proto::RpcService\">RpcService</a>"]]
+};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
