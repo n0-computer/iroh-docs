cc220ac30bb19a0cd5fe3151a8ac6ad09d366ac8
ramfox <ramfox@users.noreply.github.com>
2025-01-14 03:22:13 +0000

    deploy: a806087f83906d0e0b771ed30a99e22d44e412d0

---
+++ pr/28/docs/trait.impl/quic_rpc/pattern/try_server_streaming/trait.TryServerStreamingMsg.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["iroh_docs",[["impl TryServerStreamingMsg&lt;<a class=\"struct\" href=\"iroh_docs/rpc/proto/struct.RpcService.html\" title=\"struct iroh_docs::rpc::proto::RpcService\">RpcService</a>&gt; for <a class=\"struct\" href=\"iroh_docs/rpc/proto/struct.DocSubscribeRequest.html\" title=\"struct iroh_docs::rpc::proto::DocSubscribeRequest\">DocSubscribeRequest</a>"]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[360]}
