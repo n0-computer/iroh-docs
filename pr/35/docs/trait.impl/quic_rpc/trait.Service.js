bb2a9a5dbeae37bd60480206e38929ae3193873f
ramfox <ramfox@users.noreply.github.com>
2025-02-15 02:13:59 +0000

    deploy: f9fe3ae27103bb7e8bd292377fce931b47c268b0

---
+++ pr/35/docs/trait.impl/quic_rpc/trait.Service.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["iroh_docs",[["impl Service for <a class=\"struct\" href=\"iroh_docs/rpc/proto/struct.RpcService.html\" title=\"struct iroh_docs::rpc::proto::RpcService\">RpcService</a>"]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[174]}
