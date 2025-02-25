80a0b3b3fa32c297c2f5dadf04aaa93e35c64859
ramfox <ramfox@users.noreply.github.com>
2025-02-25 15:57:34 +0000

    deploy: 7ac8ce2291512200003233417cab3433a0471552

---
+++ pr/37/docs/iroh_docs/rpc/sidebar-items.js
@@ -0,0 +1,1 @@
+window.SIDEBAR_ITEMS = {"enum":["AddrInfoOptions"],"mod":["client","proto"]};
---
+++ pr/37/docs/trait.impl/iroh/protocol/trait.ProtocolHandler.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["iroh_docs",[["impl&lt;S: Store&gt; ProtocolHandler for <a class=\"struct\" href=\"iroh_docs/protocol/struct.Docs.html\" title=\"struct iroh_docs::protocol::Docs\">Docs</a>&lt;S&gt;"]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[186]}
