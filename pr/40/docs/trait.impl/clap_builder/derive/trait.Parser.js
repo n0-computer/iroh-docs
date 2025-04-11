66ac00003d0e021dcbc86c9b42ebdc8956ccc4a3
matheus23 <matheus23@users.noreply.github.com>
2025-04-11 07:29:35 +0000

    deploy: 76fe2216cecba14a04d85ebee8bea1b0c66de9cd

---
+++ pr/40/docs/trait.impl/clap_builder/derive/trait.Parser.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["iroh_docs",[["impl Parser for <a class=\"enum\" href=\"iroh_docs/cli/authors/enum.AuthorCommands.html\" title=\"enum iroh_docs::cli::authors::AuthorCommands\">AuthorCommands</a>"],["impl Parser for <a class=\"enum\" href=\"iroh_docs/cli/enum.DocCommands.html\" title=\"enum iroh_docs::cli::DocCommands\">DocCommands</a>"]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[325]}
