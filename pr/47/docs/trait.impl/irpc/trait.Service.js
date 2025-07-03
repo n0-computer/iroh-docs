a0f6b75d97c4f0b60d5fa38dfac4ad74c09a2243
Frando <Frando@users.noreply.github.com>
2025-07-03 10:36:55 +0000

    deploy: f41ab5f1248aa7eae508c0fe659b94a5c1da80a1

---
+++ pr/47/docs/trait.impl/irpc/trait.Service.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["iroh_docs",[["impl Service for <a class=\"struct\" href=\"iroh_docs/api/protocol/struct.DocsService.html\" title=\"struct iroh_docs::api::protocol::DocsService\">DocsService</a>"]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[183]}
