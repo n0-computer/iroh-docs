879419773629caa0313439ca755a9c27d8861efb
Frando <Frando@users.noreply.github.com>
2026-04-15 12:30:41 +0000

    deploy: c5ffa4de221519452525737f073e4b30b105bdc3

---
+++ pr/95/docs/trait.impl/core/iter/traits/collect/trait.FromIterator.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["iroh_docs",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/iter/traits/collect/trait.FromIterator.html\" title=\"trait core::iter::traits::collect::FromIterator\">FromIterator</a>&lt;(<a class=\"primitive\" href=\"https://doc.rust-lang.org/nightly/std/primitive.u64.html\">u64</a>, <a class=\"struct\" href=\"iroh_docs/struct.AuthorId.html\" title=\"struct iroh_docs::AuthorId\">AuthorId</a>)&gt; for <a class=\"struct\" href=\"iroh_docs/struct.AuthorHeads.html\" title=\"struct iroh_docs::AuthorHeads\">AuthorHeads</a>"],["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/iter/traits/collect/trait.FromIterator.html\" title=\"trait core::iter::traits::collect::FromIterator\">FromIterator</a>&lt;(<a class=\"struct\" href=\"iroh_docs/struct.AuthorId.html\" title=\"struct iroh_docs::AuthorId\">AuthorId</a>, <a class=\"primitive\" href=\"https://doc.rust-lang.org/nightly/std/primitive.u64.html\">u64</a>)&gt; for <a class=\"struct\" href=\"iroh_docs/struct.AuthorHeads.html\" title=\"struct iroh_docs::AuthorHeads\">AuthorHeads</a>"]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[1085]}
