15fa8faa06143b27b0ecd3cef759b81a62de5158
dignifiedquire <dignifiedquire@users.noreply.github.com>
2026-05-27 08:44:29 +0000

    deploy: 7f8141648c7f875545258fddf9f7df6691f8a3f0

---
+++ pr/103/docs/search.index/crateNames/d49bdf7420ea.js
@@ -0,0 +1,1 @@
+rd_("iiroh_docs")
---
+++ pr/103/docs/trait.impl/futures_core/stream/trait.Stream.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["iroh_docs",[["impl Stream for <a class=\"struct\" href=\"iroh_docs/api/struct.ImportFileProgress.html\" title=\"struct iroh_docs::api::ImportFileProgress\">ImportFileProgress</a>"]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[184]}
