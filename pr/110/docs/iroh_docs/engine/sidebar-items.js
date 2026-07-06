cf7109d614b4df4136c54d01a4bf89aa2fcc56fe
Frando <Frando@users.noreply.github.com>
2026-07-06 07:45:53 +0000

    deploy: 40bc51afc77eb312566d71bef2ddc40a42fdc7aa

---
+++ pr/110/docs/iroh_docs/engine/sidebar-items.js
@@ -0,0 +1,1 @@
+window.SIDEBAR_ITEMS = {"enum":["DefaultAuthorStorage","LiveEvent","Origin","SyncReason"],"struct":["DefaultAuthor","Engine","ProtectCallbackHandler","SyncEvent"]};
---
+++ pr/110/docs/trait.impl/core/ops/drop/trait.Drop.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["iroh_docs",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/ops/drop/trait.Drop.html\" title=\"trait core::ops::drop::Drop\">Drop</a> for <a class=\"struct\" href=\"iroh_docs/actor/struct.SyncHandle.html\" title=\"struct iroh_docs::actor::SyncHandle\">SyncHandle</a>"],["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/ops/drop/trait.Drop.html\" title=\"trait core::ops::drop::Drop\">Drop</a> for <a class=\"struct\" href=\"iroh_docs/store/fs/struct.Store.html\" title=\"struct iroh_docs::store::fs::Store\">Store</a>"]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[569]}
