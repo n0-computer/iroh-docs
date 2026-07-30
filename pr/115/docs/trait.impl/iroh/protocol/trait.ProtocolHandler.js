1b66c49059a3400172de9f502cb1c53cab303a6c
flub <flub@users.noreply.github.com>
2026-07-30 10:29:32 +0000

    deploy: 7386754f47a42da6dc1f5a73d400cddb44b2f9f5

---
+++ pr/115/docs/trait.impl/iroh/protocol/trait.ProtocolHandler.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["iroh_docs",[["impl ProtocolHandler for <a class=\"struct\" href=\"iroh_docs/protocol/struct.Docs.html\" title=\"struct iroh_docs::protocol::Docs\">Docs</a>"]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[161]}
