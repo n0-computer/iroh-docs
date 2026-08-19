e7e66593a9ee72d0f069d8c57317db3c039aa46e
Frando <Frando@users.noreply.github.com>
2026-08-19 13:57:48 +0000

    deploy: 1cfdc27f6ab48e00c76b2a4cc72bfdbc2f300926

---
+++ pr/119/docs/trait.impl/core/future/future/trait.Future.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["iroh_docs",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/future/future/trait.Future.html\" title=\"trait core::future::future::Future\">Future</a> for <a class=\"struct\" href=\"iroh_docs/api/struct.ImportFileProgress.html\" title=\"struct iroh_docs::api::ImportFileProgress\">ImportFileProgress</a>"]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[332]}
